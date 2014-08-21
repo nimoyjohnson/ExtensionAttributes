aa3dd9d40e5fcd99c81fc2442026f42cbe4f6504
quedayone <qday1@me.com>
2014-08-21 11:04:42 -0500

    Create ParallelsVersion.sh

---
+++ ParallelsVersion.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ -e /Applications/Parallels\ Desktop.app ]; then
+ParallelsV=`defaults read /Applications/Parallels\ Desktop.app/Contents/Info.plist CFBundleShortVersionString`
+ParallelsMV=`defaults read /Applications/Parallels\ Desktop.app/Contents/Info.plist CFBundleVersion`
+
+IntParallelsVersion=`echo "$ParallelsV$ParallelsMV" | sed 's/\.//g' | cut -d ' ' -f1`
+else
+IntParallelsVersion="9999999999"
+fi
+
+echo "<result>$IntParallelsVersion</result>"
+exit 0
