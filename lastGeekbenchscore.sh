553b3bd7e1203e68fb08afa77eb33785f42851ef
quedayone <pierce@wpierce25mini.collemcvoy.com>
2014-08-21 14:50:13 -0500

    Notes about switching to admin user

---
+++ lastGeekbenchscore.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+## Get the last Geekbenchscore
+#### Will Pierce
+#### August 21, 2014
+# has geekbench run and saved a text file?
+# See script: https://github.com/quedayone/ExtensionAttributes/blob/master/GeekbenchRunTest.sh
+# To run the test
+### Is geekbench installed?
+if [ -e /Users/Shared/Geekbench/ ]; then
+	LastGeekbenchScoreTXT=$(ls /Users/Shared/Geekbench/| sort -n -t _ -k 2 | tail -1)
+	LastGeekbenchScore=$(grep "Geekbench Score" /Users/Shared/Geekbench/$LastGeekbenchScoreTXT | sed 's/Geekbench Score//' | sed 's/^[ \t]*//' | cut -c7-11 | sed -e 's/^[ \t]*//')
+else
+LastGeekbenchScore="1"
+fi
+
+echo "<result>$LastGeekbenchScore</result>"
