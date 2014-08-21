e98bf637168488ba9d8247f70720fb7d0ad2407a
quedayone <pierce@wpierce25mini.collemcvoy.com>
2014-08-21 15:54:52 -0500

    Removed stuff

--- lastGeekbenchscore.sh
+++ lastGeekbenchscore.sh
@@ -3,15 +3,16 @@
 ## Get the last Geekbenchscore
 #### Will Pierce
 #### August 21, 2014
-# has geekbench run and saved a text file?
+
 # See script: https://github.com/quedayone/ExtensionAttributes/blob/master/GeekbenchRunTest.sh
 # To run the test
-### Is geekbench installed?
-if [ -e /Users/Shared/Geekbench/ ]; then
+# has geekbench run and saved a text file?
+if [ -d	 /Users/Shared/Geekbench/ ]; then
 	LastGeekbenchScoreTXT=$(ls /Users/Shared/Geekbench/| sort -n -t _ -k 2 | tail -1)
 	LastGeekbenchScore=$(grep "Geekbench Score" /Users/Shared/Geekbench/$LastGeekbenchScoreTXT | sed 's/Geekbench Score//' | sed 's/^[ \t]*//' | cut -c7-11 | sed -e 's/^[ \t]*//')
 else
+	echo Geekbenchscore folder not found
 LastGeekbenchScore="1"
 fi
-
 echo "<result>$LastGeekbenchScore</result>"
+exit 0
