4e52e2ef3eaa8bacb85cbc52516380797829ecb2
quedayone <pierce@wpierce25mini.collemcvoy.com>
2014-08-25 10:36:36 -0500

    This one works

--- SafariVersion.sh
+++ SafariVersion.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 if [ -e /Applications/Safari.app/ ]; then
 SafariVersion="$(defaults read /Applications/Safari.app/Contents/version.plist CFBundleShortVersionString)"
+echo $SafariVersion
 ##Convert to integer
-IntSafariVersion=`echo "$VLCVersion" | sed 's/\.//g' | cut -d ' ' -f1`
+IntSafariVersion=`echo "$SafariVersion" | sed 's/\.//g' | cut -d ' ' -f1`
 else
 IntSafariVersion="9999999999"
 fi
