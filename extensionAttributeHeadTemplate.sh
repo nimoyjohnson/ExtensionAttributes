79f97fd4cceca8877636f64538de450c6a028794
Will Pierce <core.editor>
2015-02-18 16:00:36 -0600

    added templates for head and version no update

---
+++ extensionAttributeHeadTemplate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+### C+M Extension Attribute template
+
+### Header needed for Git to Version Control Your JSS - Using Git-ception
+### https://github.com/Expedia-IT-CTE/git-ception
+
+# Name: Display Name for the Extension Attribute
+# Description: Description to display for the extension attribute.
+# Data Type: String
+# Inventory Display: User and Location
